0b6cc0808deba8beb9616a670fff0e2c856060d2
kazimali123 <155025143+kazimali123@users.noreply.github.com>
2024-04-16 19:05:47 +0200

    Add files via upload
    A company has made a couple of hackathons, many people contributed to those hackathons. The results
    of those hackathons are stored in a file. Each line in the files of the results starts with the ID of the
    hackathon (string without white spaces), then the name of the competitor (string without white spaces),
    then the results of every task the competitor solved on the scale of 1..100 (integer), the competitor
    solved at least one task!
    get the competitor who got the max score before the first failing competitor (the score is the
    average
    GRADE 3
    of all of the results of one competitor, and a competitor fails if he scored less than 40%)

---
+++ InpFile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextFile;
+
+namespace Satisfactory2
+{
+    internal class InpFile
+    {
+        public struct Competitor
+        {
+            public string id;
+            public string name;
+            public int first;
+            public int second;
+            public int third;
+
+            //public List<int> score;
+        }
+
+        TextFileReader reader;
+
+        public InpFile(string fileName)
+        {
+            reader = new TextFileReader(fileName);
+        }
+
+        public bool Read(out Competitor competitor)
+        {
+            reader.ReadString(out competitor.id);
+            reader.ReadString(out competitor.name);
+            reader.ReadInt(out competitor.first);
+            reader.ReadInt(out competitor.second);
+            return reader.ReadInt(out competitor.third);
+            //reader.ReadInt(out competitor.score);
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using TextFile;
+namespace Satisfactory2
+{
+    class Program
+    {
+        public static bool isFail(int a, int b, int c)
+        {
+            return ((a + b + c) / 3 * 100 <= 40);
+        }
+
+        public static int avg(int a,int b, int c)
+        {
+            return (a + b + c) / 3;
+        }
+        static void Main(string[] args)
+        {
+            string filepath = "C:\\Users\\alika\\source\\repos\\Satisfactory2\\input.txt";
+
+            InpFile x = new InpFile(filepath);
+            InpFile.Competitor c;
+            
+            int max = 0;
+            string name = null;
+
+            while(x.Read(out c))
+            {
+                
+                if (isFail(c.first, c.second, c.third))
+                {
+                    break;
+                }
+                
+                    
+                    int avgg = avg(c.first, c.second, c.third);
+                    if ( avgg > max)
+                    {
+                        max = avgg;
+                    name = c.name;
+                    }
+                    
+
+                
+
+
+
+            }
+            Console.WriteLine(name);
+
+           
+
+
+
+        }
+    }
+}
